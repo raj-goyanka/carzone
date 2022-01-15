2ecddca841a2a90abbc246092aae81c150e4dad6
Raj Goyanka <rajgoyanka51@gmail.com>
2022-01-15 10:38:54 +0530

    Added Car Apps and Fetch the data into HomePage

--- carzone/urls.py
+++ carzone/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pages.urls')),
+    path('cars/',include('cars.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
